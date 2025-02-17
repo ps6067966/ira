9e58eb859df6a99e823ad62bd0714b236a1b88a0
Pratap Singh <ps6067966@gmail.com>
2025-02-17 17:53:55 +0530

    pss

---
+++ android/app/src/main/kotlin/com/flashcoders/ira/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flashcoders.ira
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/gemini_api_key.dart
@@ -0,0 +1,1 @@
+const String geminiApiKey = 'AIzaSyBr6-9fRfyOFLY6NIFG4hyT19GRrSvtslU';
