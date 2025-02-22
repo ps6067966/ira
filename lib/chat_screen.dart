dc40745d26199ed0201e86deb4e56ce34777198b
Pratap Singh <ps6067966@gmail.com>
2025-02-22 17:25:17 +0530

    ira design change.

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:ira/gemini_api_key.dart';
 
@@ -13,6 +14,7 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _messageController = TextEditingController();
   final List<ChatMessage> _messages = [];
+  final ScrollController _scrollController = ScrollController();
   bool _isTyping = false;
   late final GenerativeModel _model;
   late final ChatSession _chat;
@@ -42,7 +44,20 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   void dispose() {
     _messageController.dispose();
+    _scrollController.dispose();
     super.dispose();
+  }
+
+  void _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
+    });
   }
 
   Future<void> _handleSubmitted(String text) async {
@@ -58,6 +73,7 @@ class _ChatScreenState extends State<ChatScreen> {
       );
       _isTyping = true;
     });
+    _scrollToBottom();
 
     try {
       final response = await _chat.sendMessage(
@@ -74,6 +90,7 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           );
         });
+        _scrollToBottom();
       }
     } catch (e) {
       setState(() {
@@ -95,68 +112,137 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Ira'),
-        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              reverse: false,
-              padding: const EdgeInsets.all(8.0),
-              itemCount: _messages.length,
-              itemBuilder: (context, index) {
-                return _messages[index];
-              },
-            ),
+        title: const Text(
+          'Ira',
+          style: TextStyle(
+            fontWeight: FontWeight.w900,
+            fontSize: 32,
+            fontFamily: 'Arial Black',
           ),
-          if (_isTyping)
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: const Color(0xFFFFE566),
+        foregroundColor: Colors.black,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFFFF90B3), // Bright pink background
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                controller: _scrollController,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  final message = _messages[index];
+                  return message
+                      .animate()
+                      .fade(duration: const Duration(milliseconds: 300))
+                      .scale(
+                        begin: const Offset(0.8, 0.8),
+                        duration: const Duration(milliseconds: 300),
+                      );
+                },
+              ),
+            ),
+            if (_isTyping)
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 12.0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF98FF98), // Mint green
+                        border: Border.all(color: Colors.black, width: 3),
+                        borderRadius: BorderRadius.circular(12.0),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black,
+                            offset: Offset(4, 4),
+                            blurRadius: 0,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          _buildDot(),
+                          const SizedBox(width: 4),
+                          _buildDot(delay: 0.2),
+                          const SizedBox(width: 4),
+                          _buildDot(delay: 0.4),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             Container(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondaryContainer,
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    child: const Row(
-                      children: [
-                        SizedBox(
-                          width: 12,
-                          height: 12,
-                          child: CircularProgressIndicator(strokeWidth: 2),
-                        ),
-                        SizedBox(width: 8),
-                        Text('Typing...'),
-                      ],
-                    ),
+              decoration: const BoxDecoration(
+                color: Color(0xFFFFE566), // Yellow
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8,
+                    offset: Offset(0, -2),
                   ),
                 ],
               ),
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: _buildTextComposer(),
+                ),
+              ),
             ),
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).cardColor,
-            ),
-            child: _buildTextComposer(),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
+  Widget _buildDot({double delay = 0.0}) {
+    return Container(
+      width: 10,
+      height: 10,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(5),
+      ),
+    )
+        .animate(
+          onPlay: (controller) => controller.repeat(),
+        )
+        .scale(
+          duration: const Duration(milliseconds: 600),
+          begin: const Offset(1, 1),
+          end: const Offset(0.5, 0.5),
+          delay: Duration(milliseconds: (delay * 1000).toInt()),
+          curve: Curves.easeInOut,
+        );
+  }
+
   Widget _buildTextComposer() {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
       decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
-        borderRadius: BorderRadius.circular(25.0),
-        border: Border.all(color: Theme.of(context).colorScheme.outline),
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12.0),
+        border: Border.all(color: Colors.black, width: 3),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black,
+            offset: Offset(4, 4),
+            blurRadius: 0,
+          ),
+        ],
       ),
       child: Row(
         children: [
@@ -165,19 +251,45 @@ class _ChatScreenState extends State<ChatScreen> {
               controller: _messageController,
               onSubmitted: _handleSubmitted,
               decoration: const InputDecoration(
-                hintText: 'Send a message',
+                hintText: 'Type a message...',
+                hintStyle: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                ),
                 border: InputBorder.none,
                 contentPadding: EdgeInsets.all(16.0),
               ),
+              style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-          IconButton(
-            icon: const Icon(Icons.send),
-            onPressed: () {
-              if (_messageController.text.isNotEmpty) {
-                _handleSubmitted(_messageController.text);
-              }
-            },
+          Container(
+            margin: const EdgeInsets.all(4.0),
+            decoration: BoxDecoration(
+              color: const Color(0xFF98FF98), // Mint green
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Colors.black, width: 2),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black,
+                  offset: Offset(2, 2),
+                  blurRadius: 0,
+                ),
+              ],
+            ),
+            child: IconButton(
+              icon: const Icon(
+                Icons.send_rounded,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                if (_messageController.text.isNotEmpty) {
+                  _handleSubmitted(_messageController.text);
+                }
+              },
+            ),
           ),
         ],
       ),
@@ -197,71 +309,86 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 4.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
       child: Row(
         mainAxisAlignment:
             isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          if (!isUser) _buildAvatar(),
+          const SizedBox(width: 8),
           Flexible(
-            child: ConstrainedBox(
+            child: Container(
               constraints: BoxConstraints(
                 maxWidth: MediaQuery.of(context).size.width * 0.75,
               ),
-              child: InkWell(
-                onLongPress: () {
-                  _copyToClipboard(context);
-                },
-                child: Stack(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0,
-                        vertical: 10.0,
-                      ),
-                      decoration: BoxDecoration(
-                        color: isUser
-                            ? Theme.of(context).colorScheme.primaryContainer
-                            : Theme.of(context).colorScheme.secondaryContainer,
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Flexible(
-                            child: Text(
-                              text,
-                              style: TextStyle(
-                                color: isUser
-                                    ? Theme.of(context)
-                                        .colorScheme
-                                        .onPrimaryContainer
-                                    : Theme.of(context)
-                                        .colorScheme
-                                        .onSecondaryContainer,
-                              ),
-                              softWrap: true,
-                              overflow: TextOverflow.visible,
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          IconButton(
-                            icon: const Icon(Icons.copy, size: 16),
-                            onPressed: () => _copyToClipboard(context),
-                            visualDensity: VisualDensity.compact,
-                            padding: EdgeInsets.zero,
-                            constraints: const BoxConstraints(),
-                          ),
-                        ],
+              child: Column(
+                crossAxisAlignment:
+                    isUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 10.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: isUser ? const Color(0xFF98FF98) : Colors.white,
+                      border: Border.all(color: Colors.black, width: 3),
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(4, 4),
+                          blurRadius: 0,
+                        ),
+                      ],
+                    ),
+                    child: InkWell(
+                      onLongPress: () => _copyToClipboard(context),
+                      child: Text(
+                        text,
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
+          const SizedBox(width: 8),
+          if (isUser) _buildAvatar(),
         ],
+      ),
+    );
+  }
+
+  Widget _buildAvatar() {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: isUser ? const Color(0xFF98FF98) : const Color(0xFFFFE566),
+        border: Border.all(color: Colors.black, width: 2),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black,
+            offset: Offset(2, 2),
+            blurRadius: 0,
+          ),
+        ],
+      ),
+      child: Center(
+        child: Icon(
+          isUser ? Icons.person : Icons.smart_toy,
+          size: 20,
+          color: Colors.black,
+        ),
       ),
     );
   }
@@ -269,9 +396,11 @@ class ChatMessage extends StatelessWidget {
   void _copyToClipboard(BuildContext context) {
     Clipboard.setData(ClipboardData(text: text));
     ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
+      SnackBar(
         content: Text('Message copied to clipboard'),
-        duration: Duration(seconds: 2),
+        behavior: SnackBarBehavior.floating,
+        width: 200,
+        duration: const Duration(seconds: 2),
       ),
     );
   }
